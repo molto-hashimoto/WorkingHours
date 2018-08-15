81772b94a9adbbb7e374d41dbe60d64ba1c8fa5a
molto-hashimoto <hashimoto@moltocarina.co.jp>
2018-08-15 14:50:18 +0900

    ログイン方法をform+submitによるPOSTに変更

--- src/client/login.js
+++ src/client/login.js
@@ -5,19 +5,4 @@ var socketio = io.connect('http://localhost:3000');
 
 loginApp.controller('loginCtrl', ['$scope', function ($scope) {
 
-	$scope.loginAddr = "";
-	$scope.loginPw = "";
-	$scope.loginId;
-	$scope.loginName;
-
-	$scope.loginSubmit = function() {
-		socketio.emit("login", {"addr" : $scope.loginAddr, "pw" : $scope.loginPw});
-	};
-	// ログイン成功時のID受信処理
-    socketio.on("login", function(info) {
-
-		var url = "/login_request" + "?&id=" + info['id'] + "&name=" + info['name'];
-
-		document.location.href = url;
-	});
 }]);
--- src/server.js
+++ src/server.js
@@ -1,22 +1,37 @@
 // ファイル関連
-const fs   = require('fs');
-const path = require('path');
+var fs   = require('fs');
+var path = require('path');
 // web サーバ、socket
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
 // assert
-const assert = require('assert');
+var assert = require('assert');
 // mongodb
-const MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 
 // clientフォルダ内のjs、cssを提供する
 app.use(express.static('client'));
 // bodyParser初期化
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 30 * 60 * 1000
+  }
+}));
 
 // mongodbクライアント管理用
 var obj_mongodb;
@@ -44,6 +59,27 @@ http.listen(3000, function(){
   console.log('Server running at http://localhost:3000/');
 });
 
+var setLoginTimer = function(){
+  timer_id = setInterval(function(){
+    var timeoutId = [];
+    // クライアントIDのタイムアウトチェック
+    for(id of client_ids) {
+      if ((getUnixTime() - id['time']) > 5){
+        timeoutId.push(id);
+      }
+    }
+    //　タイムアウトしたIDを削除
+    for(id of timeoutId) {
+      var index = client_ids.indexOf(id);
+      client_ids.splice(index, 1);
+    }
+    // 全クライアントID削除した場合にタイマ停止
+    if (client_ids.length == 0) {
+      clearInterval(timer_id);
+    }
+  }, 1000);
+}
+
 // パス指定なし→ログイン画面返却
 app.get('/', function(req, res) {
   // クライアントから指定されたファイルをresponse (localhost:3000/client/WorkTable.htmlを指定すること)
@@ -58,29 +94,44 @@ app.get('/', function(req, res) {
   });
 });
 
-app.get('/login_request', function(req, res) {
+app.post('/login', function(req, res) {
+  // 認証
+  obj_mongodb.collection('userInfo').findOne({
+    'addr': req.body['addr'], 'pw': req.body['pw']
+  }, function(err, document){
+    assert.equal(err, null);
+    console.log("login : " + req.body['addr']);
+    // ログイン成功
+    if (document != null) {
+      req.session.user = req.body['addr'];
 
-  // ID検索
-  result = client_ids.find(function(elm){
-    return elm['ID'] == Number(req.query.id);
-  });
+      // login IDを生成
+      var loginId = Math.floor( Math.random() * 101 );
+      loginId *= Math.floor( Math.random() * 101 );
+      // UNIX時刻
+      var time = getUnixTime();
+      // クライアント情報セット
+      client_ids.push({'name': document['name'], 'ID': loginId, 'time': time});
 
-  if (result != undefined){
-    var fullPath = __dirname + '/client/WorkTable.html';
-    res.writeHead(200, {"Content-Type": mime[path.extname(fullPath)] || "text/plain"});
-    fs.readFile(fullPath, function(err, data) {
-      if (err) {
-        // エラー時の応答
-      } else {
-        res.end(data, 'UTF-8');
+      // クライアントが１件目接続した場合にタイマ開始
+      if (client_ids.length == 1) {
+        setLoginTimer();
       }
-    });
-  }
-  else {
-    res.end("login error")
-  }
+      // 勤怠ページ返却
+      var fullPath = __dirname + '/client/WorkTable.html';
+      res.writeHead(200, {"Content-Type": mime[path.extname(fullPath)] || "text/plain"});
+      fs.readFile(fullPath, function(err, data) {
+        if (err) {
+          // エラー時の応答
+        } else {
+          res.end(data, 'UTF-8');
+        }
+      });
+    }
+  })
 });
 
+// 上記以外のURL指定された場合
 app.use(function(req, res, next){
 	res.status(404);
 	res.end('my notfound! : ' + req.path);
@@ -91,49 +142,6 @@ io.sockets.on("connection", function (socket) {
 
   socket.on('disconnect', function(data) {
     
-  });
-
-  socket.on("login", function(info){
-    // 認証
-    obj_mongodb.collection('userInfo').findOne({
-      'addr': info['addr'], 'pw': info['pw']
-    }, function(err, document){
-      assert.equal(err, null);
-      console.log("login : " + info['addr']);
-      // ログイン成功
-      if (document != null) {
-        // login IDを生成
-        var loginId = Math.floor( Math.random() * 101 );
-        loginId *= Math.floor( Math.random() * 101 );
-        // UNIX時刻
-        var time = getUnixTime();
-        client_ids.push({'name': document['name'], 'ID': loginId, 'time': time});
-        // ログインIDをクライアントに送信
-        socket.emit("login", {'name': document['name'], 'id': loginId});
-
-        // クライアントが１件目接続した場合にタイマ開始
-        if (client_ids.length == 1) {
-            timer_id = setInterval(function(){
-            var timeoutId = [];
-            // クライアントIDのタイムアウトチェック
-            for(id of client_ids) {
-              if ((getUnixTime() - id['time']) > 5){
-                timeoutId.push(id);
-              }
-            }
-            //　タイムアウトしたIDを削除
-            for(id of timeoutId) {
-              var index = client_ids.indexOf(id);
-              client_ids.splice(index, 1);
-            }
-            // 全クライアントID削除した場合にタイマ停止
-            if (client_ids.length == 0) {
-              clearInterval(timer_id);
-            }
-          }, 1000);
-        }
-      }
-    })
   });
 
   // クライアント起動時にユーザ名、年、月を受信
